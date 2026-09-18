40a5ee33976f4eb4053b344849af8639ed48f821
Paola <paolajohanaalmansaperez@gmail.com>
2026-09-18 17:35:45 -0500

    test(application): add create Proyecto tests

---
+++ src/test/java/com/jcaa/udec/collections/application/service/CrearProyectoServiceTest.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jcaa.udec.collections.application.service;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CrearProyectoServiceTest {
+    
+}
